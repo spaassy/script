9d52f0db1aced6e40bbe7aa2cf9577e080047028
getYourElementById <zhyf0310@163.com>
2019-09-02 15:39:35 +0800

    提交

--- bin/index.js
+++ bin/index.js
@@ -20,13 +20,14 @@ const spawn = require('react-dev-utils/crossSpawn');
 const args = process.argv.slice(2);
 
 const scriptIndex = args.findIndex(
-  x => x === 'build' || x === 'start'|| x === 'buildSub'
+  x => x === 'build' || x === 'start' || x === 'buildSub' || x === 'portalPublish'
   // x => x === 'build' || x === 'eject' || x === 'start' || x === 'test'
 );
 const script = scriptIndex === -1 ? args[0] : args[scriptIndex];
 const nodeArgs = scriptIndex > 0 ? args.slice(0, scriptIndex) : [];
 
 switch (script) {
+  case 'portalPublish':
   case 'buildSub':
   case 'build':
   case 'start': {
---
+++ bin/portalPublish.js
@@ -0,0 +1,77 @@
+const portal = require('../../../_spaassyProralConfig')
+const fs = require('fs')
+
+// 读取文件方法
+const readFileSync = (path) => {
+    return fs.readFileSync(path, 'utf8')
+}
+
+// 写入文件方法
+const writeFileSync = (path, data) => {
+    fs.writeFileSync(path, data, {
+        encoding: 'utf8',
+        flag: 'w'
+    });
+}
+
+// 读取portald的html文件 保存起来
+let portalHtmlData = readFileSync(portal.protalTarget)
+
+const injectLink = '<!-- inject link here -->'
+const injectScript = '<!-- inject script here -->'
+
+portal.subProject.map((item, index) => {
+    const data = readFileSync(item.target)
+    const resource = {
+        css: [],
+        js: []
+    }
+    if (!item.resourcePattern) {
+        return
+    }
+    item.resourcePattern.map((sub, current) => {
+        const strarry = sub.split('/')
+        const len = strarry.length
+        let regExp = null
+        let type = strarry[len - 1].split('.')[1]
+
+        if (type == 'css') {
+            regExp = new RegExp("\<link href=\"" + `${item.host}${sub}` + "\" rel=\"stylesheet\"\>")
+        }
+        if (type == 'js') {
+            regExp = new RegExp("\<script type=\"text\/javascript\" src=\"" + `${item.host}${sub}` + "\"></script>")
+        }
+        const matchStr = data.match(regExp)
+        resource[type].push(...matchStr)
+    })
+
+    let addLink = ''
+    let addScript = ''
+    resource.css.map((sucCss, indexCss) => {
+        addLink = addLink + sucCss + '\n'
+    })
+    addLink = addLink + '\t' + injectLink + '\n'
+
+    resource.js.map((sucJs, indexJs) => {
+        addScript = addScript + sucJs + '\n'
+    })
+    addScript = addScript + '\t' + injectScript + '\n'
+
+    // 修改portalhtml文件
+    portalHtmlData = portalHtmlData.replace(injectLink, addLink)
+    portalHtmlData = portalHtmlData.replace(injectScript, addScript)
+
+    let isErr = null
+
+    try {
+        writeFileSync(portal.protalTarget, portalHtmlData)
+    } catch (err) {
+        isErr = err
+        console.log(`${item.projectName} 注入失败！`)
+        console.log(err)
+    }
+
+    if(!isErr){
+        console.log(`${item.projectName} 注入成功！`)
+    }
+})
